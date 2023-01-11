b1cb19170da82bf59f57c4b646b6612c4501d8ec
jpcaulfi <joe.caulfield@solana.org>
2023-01-10 21:04:43 -0600

    beginner crash course workshop - program

--- workshops/beginner-crash-course/hello-world/program/src/lib.rs
+++ workshops/beginner-crash-course/hello-world/program/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use solana_program::{
+    account_info::{AccountInfo, next_account_info}, 
+    entrypoint, 
+    entrypoint::ProgramResult, 
+    msg, 
+    pubkey::Pubkey,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// Tells Solana that the entrypoint to this program
+//  is the "hello_world" function.
+//
+entrypoint!(hello_world);
+
+
+fn hello_world(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+
+    msg!("Hello, Solana!");
+    msg!("Our program's Program ID: {}", &program_id);
+
+    let accounts_iter = &mut accounts.iter();
+    let payer = next_account_info(accounts_iter)?;
+
+    msg!("Payer Address: {}", payer.key);
+
+    Ok(())
 }
