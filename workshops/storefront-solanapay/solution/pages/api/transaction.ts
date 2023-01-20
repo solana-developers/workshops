a4f91f870d5d27686619a393feb371bbcae7be43
Valentin Madrid <valentin@incility.com>
2023-01-20 22:11:58 +0100

    🍉 - Fix Transaction Endpoint

--- workshops/storefront-solanapay/solution/pages/api/transaction.ts
+++ workshops/storefront-solanapay/solution/pages/api/transaction.ts
@@ -1,79 +1,88 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { clusterApiUrl, Connection, Keypair, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
-import { createTransferCheckedInstruction, getAccount, getAssociatedTokenAddress, getMint } from '@solana/spl-token';
+import {
+  createTransferCheckedInstruction,
+  getAccount,
+  getAssociatedTokenAddress,
+  getMint,
+} from '@solana/spl-token';
 
-
-
-const MERCHANT_WALLET = new PublicKey('va1yPZsd2qieP5pE6gtxvAHkHKEW3qmtoZy3oN1GcBX');
+const MERCHANT_WALLET = new PublicKey(
+  'va1yPZsd2qieP5pE6gtxvAHkHKEW3qmtoZy3oN1GcBX'
+);
 const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
 
 type POST = {
-  transaction: string
-  message: string
-}
+  transaction: string;
+  message: string;
+};
 
 type GET = {
-  label: string
-  icon: string
-}
+  label: string;
+  icon: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    return get(req, res)
+    return get(req, res);
   }
 
   if (req.method === 'POST') {
-    return post(req, res)
+    return post(req, res);
   }
 }
 
 const get = async (req: NextApiRequest, res: NextApiResponse<GET>) => {
   const label = 'Solami Pizza';
-  const icon = 'https://exiledapes.academy/wp-content/uploads/2021/09/X_share.png';
+  const icon =
+    'https://media.discordapp.net/attachments/964525722301501477/978683590743302184/sol-logo1.png';
 
   res.status(200).json({
-      label,
-      icon,
- });
-}
+    label,
+    icon,
+  });
+};
 
 const post = async (req: NextApiRequest, res: NextApiResponse<POST>) => {
-   // Account provided in the transaction request body by the wallet.
-   const accountField = req.body?.account;
-   if (!accountField) throw new Error('missing account');
+  // Account provided in the transaction request body by the wallet.
+  const accountField = req.body?.account;
+  if (!accountField) throw new Error('missing account');
 
-   const sender = new PublicKey(accountField);
+  const sender = new PublicKey(accountField);
 
-   // create spl transfer instruction
-   const solTransfer = await SystemProgram.transfer({
-        fromPubkey: sender,
-        toPubkey: MERCHANT_WALLET,
-        lamports: 1000000000,
-    });
+  // create spl transfer instruction
+  const solTransfer = await SystemProgram.transfer({
+    fromPubkey: sender,
+    toPubkey: MERCHANT_WALLET,
+    lamports: 1000000000,
+  });
 
+  // create the transaction
+  const transaction = new Transaction();
+  const latestBlockhash = await connection.getLatestBlockhash();
+  transaction.feePayer = sender;
+  transaction.recentBlockhash = latestBlockhash.blockhash;
 
+  // add the instruction to the transaction
+  transaction.add(solTransfer);
 
-   // create the transaction
-   const transaction = new Transaction();
+  // Serialize and return the unsigned transaction.
+  const serializedTransaction = transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  });
 
-   // add the instruction to the transaction
-   transaction.add(solTransfer);
+  const base64Transaction = serializedTransaction.toString('base64');
+  const message = 'Thanks for buying a Solami!';
 
-   // Serialize and return the unsigned transaction.
-   const serializedTransaction = transaction.serialize({
-       verifySignatures: false,
-       requireAllSignatures: false,
-   });
-
-   const base64Transaction = serializedTransaction.toString('base64');
-   const message = 'Thanks for buying a Solami!';
-
-   res.status(200).send({ transaction: base64Transaction, message });
-}
-
+  res.status(200).send({ transaction: base64Transaction, message });
+};
