8f08d301b2de9e2400d47d561971aea83fdf1c63
jpcaulfi <joe.caulfield@solana.org>
2023-01-22 08:30:21 +0000

    layout changes

--- workshops/storefront-solanapay/solution/app/page.tsx
+++ workshops/storefront-solanapay/solution/app/page.tsx
@@ -1,105 +1,92 @@
 "use client" // this makes next know that this page should be rendered in the client
-import { useState } from "react"
-import Image from 'next/image'
+import { useRef, useState } from "react"
 import PayQR from "@/src/components/PayQR"
 
+
+type AddonItem = { name: string, price: number, amount: number }
+
 export default function Home() {
-  const [pizzaScreen, setPizzaScreen] = useState(true)
-  const [pizzaAddons, setPizzaAddons] = useState(
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [pizzaAddons, setPizzaAddons] = useState<AddonItem[]>(
     [
-      {
-        name: "Pepperoni",
-        amount: 0,
-
-      },
-      {
-        name: "Mushrooms",
-        amount: 0,
-      },
-      {
-        name: "Olives",
-        amount: 0,
-      }
-  ]
+      { name: "Pepperoni", price: 0.5, amount: 0 },
+      { name: "Mushrooms", price: 0.25, amount: 0 },
+      { name: "Olives", price: 0.25, amount: 0 },
+    ]
   )
+  const [ total, setTotal ] = useState(0)
 
-  const addonPricings = {
-    Pepperoni: 0.5,
-    Mushrooms: 0.25,
-    Olives: 0.25
-  }
-
- const [ total, setTotal ] = useState(0)
-
-
-
-  const addAddon = (addonName:string) => {
-    // when this function is called, add 1 to the amount of the addon that was clicked and update the total
+  const addAddon = (addonToAdd: AddonItem) => {
     const newPizzaAddons = pizzaAddons.map((addon) => {
-      if (addon.name === addonName) {
+      if (addon.name === addonToAdd.name) {
         addon.amount += 1
       }
       return addon
-    }
-    )
+    })
     setPizzaAddons(newPizzaAddons)
-    setTotal(total + addonPricings[addonName])
+    setTotal(total + addonToAdd.price)
+  }
 
+  const subtractAddon = (addonToAdd: AddonItem) => {
+    let minZeroFlag = false;
+    const newPizzaAddons = pizzaAddons.map((addon) => {
+      if (addon.name === addonToAdd.name) {
+        if (addon.amount === 0) {
+          minZeroFlag = true
+        } else {
+          addon.amount -= 1
+        }
+      }
+      return addon
+    })
+    if (!minZeroFlag) {
+      setPizzaAddons(newPizzaAddons)
+      setTotal(total - addonToAdd.price)
+    }
   }
 
   return (
-    // a main component for a pizza shop with a funny and playful background design in tailwindcss
     <main className="min-h-screen bg-red-500 p-2">
-        <h1 className="text-4xl font-bold text-center">Pizza Shop</h1>
-{pizzaScreen ? (
-        <div className="flex flex-col md:flex-row justify-center">
-          <div className="w-full md:w-1/2">
-            <div className="bg-white shadow-md rounded-2xl my-6 mr-2 overflow-hidden">
-              <Image width={1000} height={1000} className="w-full" src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Pizza" />
-              <div className="text-center px-3 pb-6 pt-2">
-                <h1 className="text-xl font-bold">Pizza</h1>
-                <p className="text-sm text-gray-600">A delicious pizza with the following ingredients:</p>
-                <ul className="text-sm text-gray-600">
-                  {pizzaAddons.map((addon) => (
-                    <li key={addon.name}>{addon.name} x {addon.amount}</li>
-                  ))}
-                </ul>
-                <h2>Total amount : {total}€</h2>
-                <button onClick={() => setPizzaScreen(false)} className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full my-3">
-                  Proceed to Payment
-                </button>
-              </div>
-            </div>
+      <div className="w-full">
+        <div className="flex flex-col justify-center">
+        
+          {/* Header */}
+          <div className="bg-gray-300 border-l-black mx-auto w-fit p-4 rounded-xl text-center">
+            <h1 className="text-4xl text-center m-auto font-light">Valentin&apos;s Pizza Shop</h1>
           </div>
-          <div className="w-full md:w-1/2">
-            <div className="bg-white shadow-md rounded-2xl my-6 ml-2">
-              <div className="text-center px-3 pb-6 pt-2">
-                <h1 className="text-xl font-bold">Addons</h1>
-                <p className="text-sm text-gray-600">Add some extra ingredients to your pizza:</p>
-                <ul className="text-sm text-gray-600">
-                  {pizzaAddons.map((addon) => (
-                    <li key={addon.name}>
-                      <button className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full my-3" onClick={() => addAddon(addon.name)}>
-                        {addon.name}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-) : (
-        <div className="flex flex-col h-screen my-auto items-center bgimg bg-cover">
-          
-          <PayQR />
-          <button onClick={() => setPizzaScreen(true)} className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full my-3">
-            Edit Order
-          </button>
-          </div>
-)}
-    </main>
-  
 
+          {/* Order Builder */}
+          <div className="bg-white shadow-md rounded-2xl my-6 mx-auto w-96">
+            <div className="text-center px-3 pb-6 pt-2">
+              {/* <h1 className="text-xl font-bold">Pizza</h1> */}
+              <p className="text-sm text-gray-700 my-4">One delicious pizza with the following ingredients:</p>
+              <ul className="text-sm text-gray-600">
+                {pizzaAddons.map((addon) => {
+                  return (
+                    <li 
+                      className="my-2 flex flex-row justify-left mx-10 text-lg"
+                      key={addon.name}
+                    >
+                      <p className="font-bold">{addon.name}</p>
+                      <p className="font-bold ml-auto text-red-600">{addon.amount}</p>
+                      <button className="ml-6" onClick={() => addAddon(addon)}><span>+</span></button>
+                      <button className="ml-4 mr-4" onClick={() => subtractAddon(addon)}><span>-</span></button>
+                    </li>
+                  )
+                })}
+              </ul>
+              <h2 className="my-8 text-2xl">Order Total : <span className="front-heavy text-blue-600">{total}€</span></h2>
+            </div> 
+          </div>
+
+          {/* Pay QR */}
+          { total != 0 && (
+            <PayQR />
+          )}
+        </div>
+      </div>
+    </main>
   )
 }
--- workshops/storefront-solanapay/solution/src/components/PayQR.tsx
+++ workshops/storefront-solanapay/solution/src/components/PayQR.tsx
@@ -1,37 +1,60 @@
-import { Cluster, clusterApiUrl, Connection, PublicKey, Keypair } from '@solana/web3.js';
-import { encodeURL, createQR, TransactionRequestURLFields } from '@solana/pay';
-import BigNumber from 'bignumber.js';
-import { useEffect, useRef } from 'react';
+import { encodeURL, createQR } from '@solana/pay';
+import { useEffect, useRef, useState } from 'react';
+
+const pizzeriaPubkey = "7aU2utTfmNLxUhcQytiEELFdpZ6HXTdjpEXi6RmLRF8N";
+
+type SupportedSplToken = { symbol: string, address: null | string, color: string }
 
 const PayQR = () => {
-    const ref = useRef<HTMLDivElement>(null);
 
-    const networkConfiguration = 'devnet'
-    
+  const sol = { symbol: "SOL", address: null, color: "green-600" }
+  const usdc = { symbol: "USDC", address: "", color: "blue-600" }
+  const bonk = { symbol: "BONK", address: "", color: "orange-400" }
+  const availableTokens: SupportedSplToken[] = [sol, usdc, bonk]
 
-    useEffect(() => {
-    const { location } = window
-    const reference = Keypair.generate().publicKey
-    const apiUrl = `${location.protocol}//${location.host}/api/transaction?reference=${reference.toBase58()}`
-    const urlParams: TransactionRequestURLFields = {
-      link: new URL(apiUrl)
-    };
-    const solanaUrl = encodeURL(urlParams);
-    console.log(solanaUrl)
-    const qr = createQR(solanaUrl, 512, 'transparent')
+  const [currentTokenSelection, setCurrentTokenSelection] = useState<SupportedSplToken>(sol)
+  
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const apiUrl = `${window.location.protocol}//${window.location.host}/api/transaction?reference=${pizzeriaPubkey}`
+    const qr = createQR(encodeURL({ link: new URL(apiUrl) }), 360, 'transparent')
     qr.update({ backgroundOptions: { round: 1000 } });
     if (ref.current) {
       ref.current.innerHTML = ''
       qr.append(ref.current)
     }
-    }, []); 
+  }, [])
 
-    
-    return (
-<div className='mt-5'>
-    <div ref={ref} className='rounded-xl overflow-hidden'></div>
-</div>
-    )
+  return (
+    <div className='bg-white shadow-md rounded-2xl w-auto text-center flex flex-col justify-between mx-auto'>
+      <div className="justify-self-start m-2 mt-4">
+          <p>Select an SPL Token to pay with:</p>
+          <li
+            className="flex flex-row justify-between mx-10 text-xl my-4"
+          >
+            {availableTokens.map((token) => {
+              return (
+                <button 
+                  key={token.symbol}
+                  className={`rounded-lg border-solid border border-gray-500 bg-${token.color} p-2 bg-opacity-${currentTokenSelection.symbol === token.symbol ? 20 : 60}`}
+                  onClick={() => setCurrentTokenSelection(token)}
+                  disabled={currentTokenSelection.symbol === token.symbol}
+                >
+                  {token.symbol}
+                </button>
+              )
+            })}
+          </li>
+          {currentTokenSelection && (
+            <p className='my-auto'>
+              Current selection: <span className={`font-bold text-xl text-${currentTokenSelection.color}`}>{currentTokenSelection.symbol}</span>
+            </p>
+          )}
+      </div>
+      <div ref={ref} className='rounded-xl overflow-hidden'></div>
+    </div>
+  )
 
 }
 
